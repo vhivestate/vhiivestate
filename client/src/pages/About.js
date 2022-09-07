d9e5924657a8c146d59d7df31518f4edd4651be6
vhivestate <garcia.valeria001@gmail.com>
2022-09-06 18:52:12 -0600

    nav style

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Photo from '../headshot.png'
 function About() {
   return (
-    <div>About</div>
+<div className='about'>
+  <div className='Avatar'>
+    <img src={Photo}></img>
+  </div>
+
+  
+    <p>
+    My name is Valeria Garcia. Based out of Salt Lake City, Utah. Graduated from the University of Utah. Full-stack web developer passionate about computer science. Educated in technologies such as JavaScript, MongoDB, Express.js, React, & Node.js.
+    </p>
+  </div>
+
   )
 }
 
