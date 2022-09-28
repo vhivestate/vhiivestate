a0af0d87d2bfc024b8a495a0a00ec584a5a02388
vhivestate <garcia.valeria001@gmail.com>
2022-09-28 12:26:47 -0600

    portfolio route

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Photo from '../headshot.png'
-import Trim from '../assets/trimdown.png'
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
@@ -8,46 +8,12 @@ function About() {
   <div className="card lg:card-side bg-base-100 shadow-xl">
     <figure><img src={Photo} alt="Album"/></figure>
     <div className="card-body">
-      <p> My name is Valeria. I'm a full-stack web developer based out of Salt Lake City, Utah.</p>
-      <h2 className="card-title">My Projects</h2>
-      <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={Trim} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+      <h2 className="card-title">About me</h2>
+        <p className='text-center'> My name is Valeria Garcia. I'm a full-stack web developer based out of Salt Lake City, Utah. I graduated from the University of Utah. Educated in technologies such as JavaScript, MongoDB, Express.js, React.js, and Node.js </p>
+        <button className="btn btn-active"><Link to={'/projects'}>View Portfolio</Link></button>    
+        </div>
     </div>
   </div>
-</div>
-
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-  </div>
-
   )
 }
 
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -7,32 +7,38 @@ function Projects() {
     <div className='projectPage'>
     <h1>My Projects</h1>
     <div className='projects '>
-        <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box ">
-        <div className="carousel-item max-w-fit">
-           <a href='https://github.com/GerusHays/Trim-Down'> <h2>Trim Down</h2>
-            <img src={Trim}></img></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div> 
-        <div className="carousel-item max-w-fit">
-             <a href='https://placeimg.com/250/180/arch"'> <img src="https://placeimg.com/250/180/arch" className="rounded-box" /></a>
-        </div>
-        </div>
-        </div>
-        </div>
+       <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full">
+          <img src={Trim} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide4" className="btn btn-circle">❮</a> 
+               <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+          </div> 
+          <div id="slide2" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide1" className="btn btn-circle">❮</a> 
+               <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+          </div> 
+          <div id="slide3" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide2" className="btn btn-circle">❮</a> 
+               <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+          </div> 
+          <div id="slide4" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide3" className="btn btn-circle">❮</a> 
+               <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+          </div>
+          </div>
+       </div>
+     </div>
   )
 }
 
