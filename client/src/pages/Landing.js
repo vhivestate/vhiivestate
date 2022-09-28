fed5a753b4df83e8e1538176da2b1de4461423b6
vhivestate <garcia.valeria001@gmail.com>
2022-09-27 20:35:10 -0600

    landing styling

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -8,7 +8,10 @@ function Landing() {
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
           <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <div className='flex justify-center space-x-4'  >
           <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
       </div>
     </div>
