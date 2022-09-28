586b70db8a641d78933b3df55efdcbc28f1f75e8
vhivestate <garcia.valeria001@gmail.com>
2022-09-27 20:11:30 -0600

    footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,19 +11,26 @@ import {TbBrandDiscord} from 'react-icons/tb'
 function Footer() {
     
   return (
-    <footer>
-    <div className='icons'>
-        <a href='https://twitter.com/vhivestate'> <Twitter></Twitter> </a>
+    <footer className="footer footer-center p-5 bg-base-200 text-base-content rounded">
+      <div className="grid grid-flow-col">
+        <a className="link link-hover text-lg">About us</a> 
+        <a className="link link-hover text-lg">Contact</a> 
+        <a className="link link-hover text-lg">Projects</a> 
+        <a className="link link-hover text-lg">Resume</a>
+    </div> 
+    <div>
+    <div className="grid grid-flow-col">
+    <a href='https://twitter.com/vhivestate'> <Twitter></Twitter> </a>
         <a href='https://github.com/vhivestate'> <GitHub></GitHub> </a>
         <a href='https://www.instagram.com/vhive.jpg/'> <Instagram></Instagram> </a>
         <a href="https://discord.com/channels/950224023973933116/950224023973933121/">
       <TbBrandDiscord /> </a>
         <a href='https://github.com/vhivestate'> <Linkedin></Linkedin> </a>
     </div>
-      
-      <div >
-        <p>Website by Valeria Garcia</p>
-      </div>
+  </div> 
+  <div>
+    <p>Copyright © 2022 - All right reserved by Vhivestate</p>
+  </div>
     </footer>
   )
 }
